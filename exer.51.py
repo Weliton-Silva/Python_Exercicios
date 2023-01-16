c822f075e7c2fc6b40e047842799dd8b00066cab
welit <welitonjosedasilva@gmail.com>
2023-01-16 15:59:45 -0300

    curso em video att

--- exer.51.py
+++ exer.51.py
@@ -1,3 +1,5 @@
 n1 = int(input('Digite o termo que deseja saber a progressão aritmética: '))
-for n1 in range (10, 10):
-    print()
+razao = int(input('Digite a razão: '))
+decimo = n1 + (10 - 1) * razao
+for c in range(n1, decimo + razao, razao):
+  print('{} '.format(c), end='..')

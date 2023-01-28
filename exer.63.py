9a4fac92111fc1aa4bd1021ef37badba2caa9ffb
welit <welitonjosedasilva@gmail.com>
2023-01-28 04:35:23 -0300

    curso em video att

--- exer.63.py
+++ exer.63.py
@@ -3,7 +3,17 @@ from time import sleep
 print('Sequência de Fibonacci')
 sleep(1)
 termo = int(input('Primeiro termo'))
-termo1 = termo -1
-termo2 = termo -2
-while termo > 0:
-    print('Sequencia {}'.format(termo - termo1 - termo2))
+termo1 = 0
+termo2 = 1
+fb = 0
+
+while fb == 15:
+    if fb >= termo1:
+        print('Fibonacci {}'.format(fb+termo1))
+        termo2 = +1
+else:
+    termo1 = termo2 + fb
+    print('Fibonacci {}'.format(termo1))
+
+
+
---
+++ exer.64.py
@@ -0,0 +1,11 @@
+n1 = 999
+cont = 0
+valor1 = 0
+valor = 0
+print('Digite um valor inteiro ou [ 999 ] para sair.')
+while not valor == 999:
+    valor = int(input('Digite um valor inteiro :'))
+    cont += 1
+    valor1 = valor + valor1
+print('''Foram Digitados {} valores.
+A Soma entre os valores Digitados são {}'''.format(cont - 1, valor1 - 999))

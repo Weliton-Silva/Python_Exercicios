183b21f04cfbe406341064da314d7e764a37756a
welit <welitonjosedasilva@gmail.com>
2023-01-28 05:52:06 -0300

    curso em video att

--- Criando_Menu_Opções.exer.59.py
+++ Criando_Menu_Opções.exer.59.py
@@ -1,10 +1,14 @@
+from time import sleep
 n1 =0
 val = 0
 val2 = 0
 
+val = int(input('Digite o primeiro valor: '))
+val2 = int(input('Digite o segundo valor: '))
+sleep(0.5)
+
 while not 5 <= n1:
-    val = int(input('Digite o primeiro valor: '))
-    val2 = int(input('Digite o segundo valor: '))
+    sleep(1)
     print('''Menu Inicial:
     [ 1 ] Somar
     [ 2 ] Multiplicar
@@ -18,15 +22,18 @@ while not 5 <= n1:
         print('a Multiplicação de ambos valores é {}'.format(val * val2))
     elif n1 == 3:
         if val > val2:
-            print(' O primeiro valor é o maior')
+            print('O primeiro valor {} é o maior'.format(val))
         elif val < val2:
-            print('O Segundo valor é o maior')
+            print('O Segundo valor {} é o maior'.format(val2))
         else:
             print('Valorea digitados são iguais.')
     elif n1 == 4:
         print('Digite novos Valores.')
-if n1 == 5:
-    print('Fim do programa.')
-
-
-
+        val = int(input('Digite o primeiro valor: '))
+        val2 = int(input('Digite o segundo valor: '))
+        sleep(1)
+    if n1 == 5:
+        sleep(1)
+        print('Fim do programa.')
+    else:
+        print('Opção Invalida.Tente Novamente')

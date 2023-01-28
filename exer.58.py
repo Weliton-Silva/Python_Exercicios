e30e076f9d0f875d4233df5268290bd0f6c9b990
welit <welitonjosedasilva@gmail.com>
2023-01-28 05:25:48 -0300

    curso em video att

--- exer.58.py
+++ exer.58.py
@@ -1,20 +1,19 @@
-import os
 import random
-import turtle
-from turtle import clear
-import os
 
 print('=-'*20)
 print('         Jogo de Adivinhação')
 print('=-'*20)
-tent = 0
-n1 = int(input('Digite um valor de 0 a 10: '))
 sorte = random.randint( 0, 10)
-while not n1 == sorte:
-    print('Tente acerta novamente !')
-    n1 = int(input('Digite um valor de 0 a 10: '))
-    sorte = random.randint( 0, 10)
-    tent = tent + 1
-print('Parabéns ! Você acertou,foi Necessario. {} tentativas'.format(tent+1))
-
-
+acertou = False
+tent = 0
+while not acertou:
+    n1 = int(input('Qual é o seu palpite ? '))
+    tent += 1
+    if n1 == sorte:
+        acertou = True
+    else:
+        if n1 < sorte:
+            print('Mais... Tente Novamente.')
+        elif n1 > sorte:
+            print('Menos... Tente Novamente.')
+print('Parabéns ! Você acertou,foi Necessario. {} tentativas'.format(tent))

42fc8e3231a9fe51867c5a770dec66d1d2a95419
welit <welitonjosedasilva@gmail.com>
2023-02-05 18:45:46 -0300

    curso em video att

---
+++ exer.75.py
@@ -0,0 +1,13 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite mais um número: '))
+n4 = int(input('Digite o último número: '))
+valores = n1, n2, n3, n4
+valor9 = 0
+print(f'Você Digitou os valores {valores}')
+for cont in (0, valores):
+    if valores == 9:
+
+            valor9 += 1
+print(f'O número 9 e digitado {valor9} vezes.')
+

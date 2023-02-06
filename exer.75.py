1dbe908e391f13024d8cb91f1563b721620b183f
welit <welitonjosedasilva@gmail.com>
2023-02-06 20:46:40 -0300

    curso em video att

--- exer.75.py
+++ exer.75.py
@@ -2,18 +2,24 @@ n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 n3 = int(input('Digite mais um número: '))
 n4 = int(input('Digite o último número: '))
-valores = n1, n2, n3, n4
+valores = (n1, n2, n3, n4)
 valor9 = 0
 posicao3 = 0
 pares = 0
 print(f'Você Digitou os valores {valores}')
-for cont in valores:
+#for num in range(0, len(valores)):
+   ### break
+for pos, cont in enumerate(valores):
     if cont == 9:valor9 += 1
     if cont % 2 == 0:
         pares += 1
-    posicao3 += 1
 print(f'O número 9 e digitado {valor9} vezes.')
-print(f'O número 3 aparece {posicao3} posição')
+#print(f'O número 3 aparece {posicao3} posição')
 print(f'Os valores pares digitados foram {pares}')
 
-
+for pos, cont in enumerate(valores):
+    if cont == 3:
+        print(f'O número {valores.count(3)} aparece {pos}º Posição.')
+    elif:
+        print('O número 3 não foi Digitado')
+print('Fim de exercicio')

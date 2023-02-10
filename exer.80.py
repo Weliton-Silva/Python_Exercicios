0b4b9d3903ccb0f2a49aab2ad0777712ad6dd147
welit <welitonjosedasilva@gmail.com>
2023-02-10 14:06:42 -0300

    curso em video att

--- exer.80.py
+++ exer.80.py
@@ -1,7 +1,14 @@
 valor = list()
+
 for cont in range( 0, 5):
-    valor = int(input('Digite um valor: '))
-    if cont in valor:
-
-
-
+    n1 = int(input('Digite um valor: '))
+    if cont == 0 or n1 > valor[-1]:
+        valor.append(n1)
+    else:
+        pos = 0
+        while pos < len (valor):
+            if n1 <= valor[pos]:
+                valor.insert(pos, n1)
+                break
+            pos += 1
+print(f'Os valores digitados em ordem foram {valor}')

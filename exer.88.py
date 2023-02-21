5d902ea6568b56475d5fb435a6a64e0256b30d74
welit <welitonjosedasilva@gmail.com>
2023-02-21 13:48:04 -0300

    curso em video att

--- exer.88.py
+++ exer.88.py
@@ -1,11 +1,29 @@
 from random import randint
+from time import sleep
+
 print('-'*30)
 print(f'      JOGO DA MEGA SENA')
 print('-'*30)
-sorteio = randint(0,60)
-palp = int(input('Quantos jogos deseja fazer ?'))
-jogo = [[], [], [], [], [], []]
-for c, sorteio in range(0, palp):
-    for c, sorteio in range(0, palp):
-        jogo = sorteio
-        print(jogo)
+sorteio = randint(1, 60)
+palp = int(input('Quantos jogos deseja fazer ? '))
+lista = list()
+tot = 1
+jogo = list()
+while tot <= palp:
+    cont = 0
+    while True:
+        sorteio = randint(1, 60)
+        if sorteio not in jogo:
+            jogo.append(sorteio)
+            cont +=1
+        if cont >= 6:
+            break
+    tot += 1
+    jogo.sort()
+    lista.append(jogo[:])
+    jogo.clear()
+print('-='*3, f'SORTEANDO {palp} JOGOS', '-='*3)
+for i, l in enumerate(lista):
+    print(f'jogo {i+1}: {l}')
+    sleep(1)
+print('-='*5, 'BOA SORTE', '-='*5)

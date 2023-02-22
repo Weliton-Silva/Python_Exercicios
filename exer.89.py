7b100fc2bf8e1bed860e31934dd58b9f2a60fad5
welit <welitonjosedasilva@gmail.com>
2023-02-22 01:24:35 -0300

    curso em video att

--- exer.89.py
+++ exer.89.py
@@ -1,24 +1,24 @@
-lista = list()
-nota1 = nota2 = 0
-lista2 = list()
-media = 0
+lista = []
+nota1 = nota2 = media = 0
 while True:
     nome = str(input('Nome: '))
     nota1 = float(input('Nota 1: '))
     nota2 = float(input('Nota2: '))
-    lista.append(nome)
-    lista.append(nota1)
-    lista.append(nota2)
+    media = (nota1 + nota2) / 2
+    lista.append([nome, [nota1, nota2], media])
     cont = str(input('Deseja continuar ? [S/N]')).upper().split()[0]
     if cont == 'N':
         break
-media = (nota1 + nota2) % 2
-lista2.append(nome)
-lista2.append(media)
 print('=-'*30)
-print('No. Nome        Média')
-print('--'*30)
-for n, m in enumerate(lista2):
-    print(f'{n} {m}      {media}', )
-print('=-'*30)
-print(lista)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-'*30)
+for i, a in enumerate(lista):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-'*35)
+    opc = int(input('Mostrar a nota de qual aluno ? (999 interrompe): '))
+    if opc == 999:
+        break
+    if opc <= len(lista)-1:
+        print(f'Notas de {lista[opc][0]} são {lista[opc][1]}')
+    print('FIM DO PROGRAMA')

71776e533ca0884d9b40ace8c7fcbf47dc2223de
welit <welitonjosedasilva@gmail.com>
2023-02-27 01:10:51 -0300

    curso em video att

--- exer.92.py
+++ exer.92.py
@@ -1,3 +1,16 @@
-lista = {'nome', 'idade', 'ctps', 'contratação', 'salario'}
+from datetime import date
+
+lista = {'Ano': date.today().year}
 dados = list
-lista["nome"] = str(input('Nome: '))
+ano = (date.today().year)
+lista["Nome"] = str(input('Nome: '))
+lista["Ano"] = int(input('Ano de Nascimento: '))
+lista["ctps"] = int(input('Carteira de Trabalho (0 não tem): '))
+
+if lista["ctps"] == 0:
+    print('=-'*15)
+    for v, l in lista.items():
+        print(f'{v} tem o valor {l}.')
+        if l > 10:
+elif lista["ctps"] != 0:
+    lista["Salario"] = int(input('Salário: '))

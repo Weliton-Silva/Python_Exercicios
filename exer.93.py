c800455fcec9699a2122efc0087db7fd47c9e440
welit <welitonjosedasilva@gmail.com>
2023-02-27 21:31:27 -0300

    curso em video att

--- exer.93.py
+++ exer.93.py
@@ -1,6 +1,19 @@
 dados = dict()
 lista = []
+
 dados["Nome"] = str(input('Nome do Jogador: '))
 partidas = int(input(f'Quantas partidas {dados["Nome"] } jogou ? '))
 for p in range(partidas):
     lista.append(int(input(f'Quantos gols na partida {p +1} ? ')))
+dados["Jogos"] = lista
+dados["Total"] = sum(lista)
+print('-='*20)
+print(f'{dados}, {lista},')
+print('-='*20)
+for d, v in dados.items():
+    print(f'O campo {d} tem o valor {v}')
+print('-='*20)
+print(f'O jogador {dados["Nome"]} jogou {partidas} partidas.')
+for l, g in enumerate(lista):
+    print(f'   => Na partida {l+1}º, fez {g} gols.')
+print(f'Foi um total de {dados["Total"]} gols.')
---
+++ exer.94.py
@@ -0,0 +1,33 @@
+dados = dict()
+lista = list([[], []])
+somaidade = 0
+while True:
+    nome = str(input('Nome: '))
+    Sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    idade = int(input('Idade: '))
+    somaidade = somaidade + idade
+    dados["MediaIdade"] = somaidade / len(lista)
+    if idade >= dados["MediaIdade"]:
+        dados["IdadeAcima"] = nome, Sexo, idade
+    if Sexo == 'M':
+        lista[0].append(nome)
+        lista[0]. append(Sexo)
+        lista[0].append(idade)
+
+    if Sexo == 'F':
+        lista[1].append(nome)
+        dados["NomeF"] = nome
+        lista[1].append(Sexo)
+        lista[1].append(idade)
+
+    cont = str(input('Quer continuar ? [S/N] ')).strip().upper()[0]
+    if cont == 'N':
+        break
+dados["MediaIdade"] = somaidade / len(lista)
+print('-='*30)
+print(f'O grupo tem {len(lista)} pessoas.')
+print(f'A média de idade é de {dados["MediaIdade"]} anos.')
+print(f'As mulheres cadastradas foram: {dados["NomeF"]}')
+print(f' Lista de pessoas que estão acima da média:')
+for l in dados["IdadeAcima"]:
+    print(f'{l}')

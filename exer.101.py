17fe6511eaeb5e717bc7268e19a38906e18c22da
welit <welitonjosedasilva@gmail.com>
2023-03-11 12:23:40 -0300

    curso em video att

---
+++ exer.101.py
@@ -0,0 +1,15 @@
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: Não Vota !'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: Voto Opcional.'
+    else:
+        return f'Com {idade} anos: Voto Obrigatorio.'
+
+print('--'*15)
+nasc = (int(input('Em que ano você nasceu ? ')))
+print(voto(nasc))
+print('--'*15)

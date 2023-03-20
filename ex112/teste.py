856806920f9bd5b9aa3c3fea8a10039999f2ca3d
welit <welitonjosedasilva@gmail.com>
2023-03-20 14:17:51 -0300

    curso em video att

---
+++ ex112/teste.py
@@ -0,0 +1,5 @@
+import moedas
+from ex112.utilidadescev import dado
+
+p = dado.leiaDinheiro('Digite o preço: R$ ')
+moedas.resumo(p)
---
+++ exer.111/teste.py
@@ -0,0 +1,5 @@
+import moedas
+
+
+p = float(input('Digite o preço: R$ '))
+moedas.resumo(p)

1cd6fe5753c6ae8c68d3d7a7ef7069db649adf60
welit <welitonjosedasilva@gmail.com>
2023-03-24 13:28:24 -0300

    curso em video att

---
+++ exer115/libe/arquivo/__init__.py
@@ -0,0 +1,53 @@
+from exer115.libe.interface import *
+
+
+def arquivoExiste(nome):
+    try:
+        a = open(nome, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+
+
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Houve um ERRO na criação do arquivo!')
+    else:
+        print(f'Arquivo{nome} criado com Sucesso!')
+
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler p arquivo')
+    else:
+        cabecalho('PESSOAS CADASTRADAS')
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30} {dado[1]:>3} anos')
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('Houve um erro na abertura do arquivo!')
+    else:
+        try:
+            a.write(f'{nome}; {idade}\n')
+        except:
+            print('Houve um erro na hora de escrever os dados!')
+        else:
+            print(f'Novo registro de {nome} adicionado')
+            a.close()
+
+
+
--- exer115/sistema.py
+++ exer115/sistema.py
@@ -1,15 +1,27 @@
 from exer115.libe.interface import *
 from time import sleep
+from exer115.libe.arquivo import *
+
+arq = 'cursoemvideo.txt'
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
-    resposta = menu(['opc01', 'opc02', 'opc03', 'Super opção'])
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('opção 1')
+        # Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('opção 2')
+        # Opção de cadastrar uma nova pessoa.
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabecalho('Saindo do Sistema... Até logo!')
         break
     else:
+        # Digitou uma opção errada no menu.
         print('\033[31mErro! Digite uma opção válida!\033[m')
     sleep(2)
